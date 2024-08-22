b31eee1e6cbe525dd8040deeb0dce5e6f4c80148
ChinmayNaik02 <ChinmayNaik02>
2024-08-22 23:52:23 +0530

    Made small tweaks

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -2,14 +2,16 @@ package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
-    private static final String apiKey = "4fded2211bb9e3a01dc362316140aeb5";
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
