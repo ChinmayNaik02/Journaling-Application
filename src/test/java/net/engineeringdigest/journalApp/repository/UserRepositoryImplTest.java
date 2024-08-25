9c8b213baa3668c11bb4e4737c83a1fb5f4082bc
ChinmayNaik02 <ChinmayNaik02>
2024-08-26 01:11:20 +0530

    Used Redis cloud for caching the frequently requested data

--- src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImplTest.java
+++ src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImplTest.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalApp.repository;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ public class UserRepositoryImplTest {
     @Autowired
     private UserRepositoryImpl userRepository;
 
+    @Disabled("tested")
     @Test
     public void testSaveNewUser(){
         userRepository.getUserForSA();
